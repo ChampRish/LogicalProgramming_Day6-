5599f1afb9b40e4d7c469087a6afaa8c7cd4e034
RISHABH <gguptarishabh82@gmail.com>
2022-03-09 02:22:48 +0530

    Logical programs call

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs_Day06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //fibonnaciseries.Displayfibonnaciseries();
+              //Perfect_NUmber.DisplayPerfectNumber();
+            Reversethe_Number.DisplayReverseNumber();
+            //Prime_Number.FindPrimeNumbers();
+            Console.ReadLine();
+        }
+    }
+}
