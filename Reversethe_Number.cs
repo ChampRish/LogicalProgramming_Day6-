bf8d248f4b515b11d8fca9305d41c5bc51004819
RISHABH <gguptarishabh82@gmail.com>
2022-03-09 11:42:13 +0530

    DisplayReverseNumber

---
+++ Reversethe_Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs_Day06
+{
+    public class Reversethe_Number
+    {
+        public static void DisplayReverseNumber()
+        {
+            int n;
+            int reverse = 0;
+            int rem;
+            Console.WriteLine("Enter the number to be reversed");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+
+            Console.WriteLine(" Reverse number " + reverse);
+        }
+    }
+}
